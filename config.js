4db68359046aadbee23ac78f8baddf6fe1660c59
Cuddleota <kaito_ota@cuddle-co.jp>
2026-03-16 19:03:29 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,6 @@
-  // =====================================================
-  // 設定ファイル - ここを編集してください
-  // =====================================================
-
-  const CONFIG = {
-    // Google Maps API キー
-    // https://console.cloud.google.com/ で取得
-    GOOGLE_MAPS_API_KEY: "AIzaSyBpEc3rh_6kTA9Lf31ADNooSOWILQ0TBZA",
-
-    // Google スプレッドシートのCSV公開URL
+const CONFIG = {                       
+    GOOGLE_MAPS_API_KEY: "AIzaSyBpEc3rh_6kTA9Lf31ADNooSOWILQ0TBZA",           
     SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT529vD--xDytnL1dkzadkvXVS72ZSyEDHmEGQwHqt0FVFMr33K-RnZArPXCcGdXY_V1iss8IOny3yj/pub?gid=1248405287&single=true&output=csv",
-    // 地図の初期表示設定
     MAP_CENTER: { lat: 36.2048, lng: 138.2529 },
     MAP_ZOOM: 6,
   };
